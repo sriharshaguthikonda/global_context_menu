c2bbd2321b0aec9ca016ce91d5fdaa46c45eded5
Dr. Sri Harsha Guthikonda <g.sriharsha746electronics@gmail.com>
2026-06-19 16:33:35 +0530

    Use router base activity for launcher

--- example/android/app/src/main/kotlin/com/lkrjangid/global_context_menu_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/lkrjangid/global_context_menu_example/MainActivity.kt
@@ -1,5 +1,3 @@
 package com.lkrjangid.global_context_menu_example
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
+class MainActivity : RouterFlutterActivity()
