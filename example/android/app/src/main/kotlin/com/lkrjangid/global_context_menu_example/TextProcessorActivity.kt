40b746c2208d743e03eddefca7b0b40a91b3566c
Dr. Sri Harsha Guthikonda <g.sriharsha746electronics@gmail.com>
2026-06-19 16:33:47 +0530

    Use router base activity for process text

--- example/android/app/src/main/kotlin/com/lkrjangid/global_context_menu_example/TextProcessorActivity.kt
+++ example/android/app/src/main/kotlin/com/lkrjangid/global_context_menu_example/TextProcessorActivity.kt
@@ -1,20 +1,3 @@
 package com.lkrjangid.global_context_menu_example
 
-import android.os.Bundle
-import io.flutter.embedding.android.FlutterActivity
-
-/**
- * Activity that handles the ACTION_PROCESS_TEXT intent.
- *
- * This activity is launched when a user selects text in any app
- * and chooses "Process Text" from the text selection toolbar.
- *
- * The actual text processing logic is handled by the Flutter code
- * using the GlobalContextMenu plugin.
- */
-class TextProcessorActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // The GlobalContextMenu plugin will handle the rest in the Flutter code
-    }
-}
+class TextProcessorActivity : RouterFlutterActivity()
